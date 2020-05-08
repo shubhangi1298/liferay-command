5aaab1b8e514e329514de97d4226c6d9c7b93da6
shubhangi1298 <64908206+shubhangi1298@users.noreply.github.com>
2020-05-08 12:38:26 +0530

    First

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author Shubhangi Bhargava
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "guestbook";
+
+}
